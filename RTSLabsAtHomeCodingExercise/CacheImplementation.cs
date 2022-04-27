031243680b3addb1c3924d245a6d3692b09eb468
Hermann McEnhill <hmcenhill@gmail.com>
2022-04-27 15:07:32 -0400

    add code from 2nd coding exercise

---
+++ RTSLabsAtHomeCodingExercise/CacheImplementation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rts.Interview
+{
+    public class CacheImplementation<T> : ICache<T> where T : class
+    {
+        private IDictionary<string, T> cache;
+        private int maxSize;
+
+        public CacheImplementation(int size)
+        {
+            cache = new Dictionary<string, T>();
+            SetMaxSize(size);
+        }
+
+        public T Get(string key)
+        {
+            if (cache == null)
+            {
+                return null;
+            }
+            if (cache.ContainsKey(key))
+            {
+                return cache[key];
+            }
+            return null;
+        }
+
+        public void Put(string key, T value)
+        {
+            if (cache != null)
+            {
+                if (cache.ContainsKey(key))
+                {
+                    cache[key] = value;
+                }
+                else
+                {
+                    cache.Add(key, value);
+                }
+                if (cache.Count() > GetMaxSize())
+                {
+                    EjectNext();
+                }
+            }
+        }
+
+        public int GetCurrentCacheSize() => cache.Count();
+
+        public int GetMaxSize() => this.maxSize;
+
+        public void SetMaxSize(int maxSize)
+        {
+            if(maxSize < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxSize));
+            }
+
+            if (maxSize < this.maxSize)
+            {
+                while (GetCurrentCacheSize() > maxSize)
+                {
+                    EjectNext();
+                }
+            }
+
+            this.maxSize = maxSize;
+        }
+
+        private void EjectNext()
+        {
+            if (cache.Any())
+            {
+                cache.Remove(cache.FirstOrDefault());
+            }
+        }
+    }
+
+    public class CachedItem<T>
+    {
+        public T Value { get; set; }
+        public DateTime DateAdded;
+
+        public CachedItem(T value)
+        {
+            Value = value;
+            DateAdded = DateTime.Now;
+        }
+
+        public bool AddedBefore(CachedItem<T> other)
+        {
+            return this.DateAdded < other.DateAdded;
+        }
+    }
+}
---
+++ RTSLabsAtHomeCodingExercise/ICache.cs
@@ -0,0 +1,45 @@
+﻿namespace Rts.Interview
+{
+    /// <summary>
+    /// A key-value cache with a size limit.
+    /// </summary>
+    public interface ICache<T>
+    {
+        /// <summary>
+        /// Gets the value stored by the key.
+        /// </summary>
+        /// <param name="key">The key from which to get a value.</param>
+        /// <returns>The value, or <code>null</code> if no value is cached for the given key.</returns>
+        T Get(String key);
+
+        /// <summary>
+        /// Puts the value into the cache by the key. Only one key of the same value can be cached such that the value
+        /// with the most recently put key is kept.
+        /// If putting the value would extend the cache past the maximum size limit defined by <code>SetMaxSize()</code>
+        /// Would eject item. (I get to choose)
+        /// </summary>
+        /// <param name="key">key The key for which to cache the value.</param>
+        /// <param name="value">The cached value.</param>
+        void Put(String key, T value);
+
+        /// <summary>
+        /// Gets the allowed maximum size of the cache.
+        /// </summary>
+        /// <returns>The current maximum size of the cache.</returns>
+        int GetMaxSize();
+
+        /// <summary>
+        /// Adjusts the maximum size of the cache. If the cache is resized to hold less values that are currently cached
+        /// then values must be ejected until the current size is equal to the maximum size.
+        /// </summary>
+        /// <param name="maxSize">The new maximum size of the cache.</param>
+        /// <exception name="IllegalArgumentException">If maxSize is less than or equal to 0</exception>
+        void SetMaxSize(int maxSize);
+
+        /// <summary>
+        /// Gets the number of currently cached items.
+        /// </summary>
+        /// <returns>The cached item count.</returns>
+        int GetCurrentCacheSize();
+    }
+}
